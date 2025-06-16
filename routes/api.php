93462bc944fbaf6721d8250171393f7305720ef4
shuriza <firdausmfirdaus657@gmail.com>
2025-06-16 17:47:48 +0700

    Deploy Laravel ke Azure via GitHub Actions

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/callback', [MidtransController::class, 'callback']);
+Route::get('/callback-test', function () {
+    return response()->json(['message' => 'Tunnel jalan!']);
+});
